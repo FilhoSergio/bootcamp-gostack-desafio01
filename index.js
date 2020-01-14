eaae722a402c3713c2cb7b0ddd8d0281af86512c
Sergio Filho <serginhoadeus@gmail.com>
2020-01-13 22:10:50 -0300

    implementação de um post para tasks dentro de um projeto definido por id no array e a implementação de um middleware para checar a existencia de um projeto no array

--- index.js
+++ index.js
@@ -2,30 +2,67 @@
 const express = require("express"); //Declarar express
 
 const server = express();
-server.use(express.json());
-const projects = [{ id: 1, title: "PecNordeste", tasks: [] }];
 
-// server.post("/projects/", (req, res) => {});
+server.use(express.json());
+
+const projects = [{ id: "1", title: "PecNordeste", tasks: [] }];
+function checkProjectInArray(req, res, next) {
+  const project = projects[req.params.index];
+  if (!project) {
+    return res.status(400).json({ error: "Projeto não encontrado" });
+  }
+
+  req.project = project;
+  return next();
+}
 
 server.get("/projects", (req, res) => {
   return res.json(projects);
 });
 
-server.get("/projects/:index", (req, res) => {
+server.get("/projects/:index", checkProjectInArray, (req, res) => {
   return res.json(projects);
 });
 
 server.post("/projects", (req, res) => {
-  //   const { project } = req.body;
-
   projects.push(req.body);
 
   return res.json({
-    verbose: ` ${req.body.title} Salvo com sucesso`,
+    verbose: ` ${req.body.title} foi Salvo com sucesso`,
     projects: projects
   });
 });
 
-server.put("/projects/:index", (req, res) => {});
+server.post("/projects/:index/tasks", checkProjectInArray, (req, res) => {
+  const { index } = req.params;
+  const { title } = req.body;
+  projects[index].tasks.push(title);
+
+  return res.json({
+    verbose: ` Task : ${title} foi  Salva com sucesso`,
+    projects: projects
+  });
+});
+
+server.put("/projects/:index", checkProjectInArray, (req, res) => {
+  const { index } = req.params;
+
+  projects[index] = req.body;
+  return res.json({
+    verbose: `Projeto id ${index} ,  ${req.body.title} foi Editado com Sucesso `,
+    projects: projects
+  });
+});
+
+server.delete("/projects/:index", checkProjectInArray, (req, res) => {
+  const { index } = req.params;
+
+  projects.splice(index, 1);
+
+  return res.json({
+    verbose: `Projeto id ${index} foi Removido com Sucesso `,
+    projects: projects
+  });
+});
 
 server.listen(3002);
